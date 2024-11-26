669bed0cd0f9e095d3d0a90668f9f897c08815fa
Rakibul Islam <fl.rakibul@gmail.com>
2024-11-26 22:29:31 +0600

    Add a remote uri

--- src/helpers/config/env.config.ts
+++ src/helpers/config/env.config.ts
@@ -1,3 +1,7 @@
 export const getBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:7000/api/v1/'
+  // return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:7000/api/v1/'
+  return (
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    'http://148.135.136.221:8085/api/v1/'
+  )
 }
